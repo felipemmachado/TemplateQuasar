36b7c6f2dc4a5d924fb1a708f3ba4aefb1f943d4
Felipe Machado <felipemmachado28@gmail.com>
2020-03-14 15:17:08 -0300

    criando a tela de login

--- src/quasar.js
+++ src/quasar.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 
 import './styles/quasar.scss';
+import './assets/animate.css';
 import lang from 'quasar/lang/pt-br';
 import '@quasar/extras/material-icons/material-icons.css';
 import {
@@ -24,6 +25,7 @@ import {
   QInput,
   QForm,
   QCardActions,
+  Notify,
 } from 'quasar';
 
 Vue.use(Quasar, {
@@ -52,6 +54,7 @@ Vue.use(Quasar, {
   directives: {
   },
   plugins: {
+    Notify,
   },
   lang,
 });
--- src/views/login/main.vue
+++ src/views/login/main.vue
@@ -2,53 +2,78 @@
   <div>
     <q-layout>
       <q-page-container>
-        <q-page
-          class="bg-light-green window-height window-width
-           row justify-center items-center">
-          <div class="column">
+        <q-page class="window-height window-width row justify-center items-center">
+          <div class="column animated fadeInDown">
             <div class="row">
-              <h5 class="text-h5 text-white q-my-md">Company & Co</h5>
-            </div>
-            <div class="row">
-              <q-card square bordered class="q-pa-lg shadow-1">
-                <q-card-section>
-                  <q-form class="q-gutter-md">
-                    <q-input outlined v-model="email" type="email" label="email" />
-                    <q-input outlined
-                      v-model="password" type="password" label="password" />
-                  </q-form>
-                </q-card-section>
-                <q-card-actions class="q-px-md">
-                  <q-btn unelevated
-                  color="light-green-7" size="lg" class="full-width" label="Login" />
-                </q-card-actions>
-                <q-card-section class="text-center q-pa-none">
-                  <p class="text-grey-6">Not reigistered? Created an Account</p>
-                </q-card-section>
-              </q-card>
+              <q-form class="q-gutter-m d">
+                <img class="q-pb-lg" style="width: 22em"  src="../../assets/stories.png" />
+                <q-input
+                  outlined
+                  v-model="login"
+                  ref="login"
+                  class="q-pb-lg"
+                  type="text"
+                  :rules="[val => !!val || 'O login é obrigatório']"
+                  label="login">
+                    <template v-slot:append>
+                        <q-icon name="account_circle" />
+                    </template>
+                </q-input>
+
+                <q-input
+                  v-model="password"
+                  outlined
+                  ref="password"
+                  :rules="[val => !!val || 'A senha é obrigatório']"
+                  class="q-pb-xl"
+                  type="password"
+                  label="senha">
+                    <template v-slot:append>
+                        <q-icon name="vpn_key" />
+                    </template>
+                </q-input>
+
+                <q-btn color="light-blue-7"
+                        size="lg"
+                        @click="clickLogin"
+                        class="full-width"
+                        label="Entrar" />
+              </q-form>
             </div>
           </div>
         </q-page>
       </q-page-container>
     </q-layout>
-
   </div>
 </template>
 
 <script>
+
 export default {
   data() {
     return {
-      teste: 'felipe',
-      email: '',
+      login: '',
       password: '',
     };
+  },
+  methods: {
+    clickLogin() {
+      const loginPass = this.$refs.password.validate();
+      const passwordPass = this.$refs.login.validate();
+      if (!loginPass && !passwordPass) {
+        this.$q.notify({
+          type: 'negative',
+          position: 'top-right',
+          message: 'Corrija os campos em vermelho.',
+        });
+      }
+    },
   },
 };
 </script>
 
 <style>
 .q-card {
-  width: 400px;
+  min-width: 22em;
 }
 </style>
