7ac1582101a6f47c845466129566f81cef14f2d7
Felipe Machado <felipemmachado28@gmail.com>
2020-03-16 18:01:48 -0300

    criando o component de login

--- src/router/index.js
+++ src/router/index.js
@@ -14,10 +14,14 @@ const routes = [
   {
     path: '/',
     name: 'menu',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/menu/main.vue'),
+    component: () => import('../views/menu/main.vue'),
+    children: [
+      {
+        path: '/inicio',
+        name: 'inicio',
+        component: () => import('../views/inicio/main.vue'),
+      },
+    ],
   },
 ];
 
--- src/views/menu/main.vue
+++ src/views/menu/main.vue
@@ -80,7 +80,7 @@
     </q-drawer>
 
     <q-page-container>
-      <!-- <HelloWorld /> -->
+      <router-view></router-view>
     </q-page-container>
   </q-layout>
 </template>
